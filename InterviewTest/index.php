ed76c05269e6bcc58051b9326680dae206a0c265
Ahsan Zaman <ahsanzam@usc.edu>
2018-06-18 21:35:33 -0700

    Requested changes to navbar

--- InterviewTest/index.php
+++ InterviewTest/index.php
@@ -15,7 +15,8 @@
 
 <div class="navbar">
 	<div id="app-title" class="titles">Omega</div>
-	<div id="brand"></div>
+	<div class="brand" id="left-brand"></div>
+	<div class="brand" id="right-brand"></div>
 	<!-- <img id="brand" src='./../assets/ELC_Logo2.png'> -->
 </div>
 <div id="application"></div>
@@ -28,30 +29,36 @@
 		position:relative;
 		top:0;
 		left:0;
-		padding: 5px
 	}
 	#app-title{
 		font-size:1.5em;
 		position:absolute;
 		left:0;
 		right:0;
-		top:7px;
+		top:13px;
 		text-align:center;
 	}
 	#application{
 		overflow-x:hidden;
 	}
-	#brand{
-		width:358px;
-		height:28.7px;
-		padding:3px;
-		position:relative;
-		top:5px;
-		left:0;
+	.brand{
+		height:60px;
+		position:absolute;
+		top:0;
 		cursor:pointer;
-		background-image:url('./../assets/ELC_Logo2.png');
 		background-size:contain;
 		background-repeat:no-repeat;
+	}
+	#left-brand{
+		position:relative; /*needs at least one element with position:relative to give parent div height*/
+		left:0;
+		background-image:url('./../assets/Formal_Marshall_ELC_CardOnGold.jpg');
+	}
+	#right-brand{
+		top:4px;
+		width:60px;
+		right:5px;
+		background-image:url('./../assets/JFF-ELC-Mouse-Pad-CIRCLE.png');
 	}
 	@media screen and (max-width: 800px) {
 		#body{ background-color:black; }
